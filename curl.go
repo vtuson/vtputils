2cda3ee711322b885fba986a7ad5244a67284b7d
Kyle Nitzsche <kyle.nitzsche@gcanonical.com>
2015-11-18 13:14:07 -0500

    let Curl user specify headers

--- curl.go
+++ curl.go
@@ -94,7 +94,7 @@ func (p RequestParms) populateHeaders(req *http.Request) {
 }
 
 //provides a httpResponse for a GET,DELETE,POST or PUT. Can support json data, use "json" as a key on the parmeter
-func Curl(p RequestParms) (*http.Response, error) {
+func Curl(p RequestParms, headers ...map[string]string) (*http.Response, error) {
 	var client http.Client
 	var req *http.Request
 	var resp *http.Response
@@ -113,9 +113,15 @@ func Curl(p RequestParms) (*http.Response, error) {
 		if err != nil {
 			log.Println(err)
 		}
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
-		p.populateHeaders(req)
+		if len(headers) > 0 {
+			for k, v := range headers {
+				req.Header.Add(k, v)
+			}
+		} else {
+			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+			req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+			p.populateHeaders(req)
+		}
 		resp, err = client.Do(req)
 	}
 	if p.Method == HTTP_JSONPOST {
